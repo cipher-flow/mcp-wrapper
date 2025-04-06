83ad216b29a609c6edf1a7a2a4515c9b38257f9e
Harry <2411mail@gmail.com>
2025-04-06 11:54:08 +0800

    chore: put invite code configs to env

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
+import 'dotenv/config';
+
+const validateEnvVar = (name, defaultValue) => {
+  const value = parseInt(process.env[name] || defaultValue);
+  if (isNaN(value) || value < 0) {
+    throw new Error(`Invalid ${name} configuration`);
+  }
+  return value;
+};
+
 export const config = {
   inviteCode: {
-    length: 12, // Length of generated invite codes
-    maxServers: 10, // Maximum number of servers per invite code
-    maxAccesses: 1000, // Maximum number of accesses per invite code
+    length: validateEnvVar('INVITE_CODE_LENGTH', 12),
+    maxServers: validateEnvVar('INVITE_CODE_MAX_SERVERS', 100),
+    maxAccesses: validateEnvVar('INVITE_CODE_MAX_ACCESSES', 1000),
   }
 };
